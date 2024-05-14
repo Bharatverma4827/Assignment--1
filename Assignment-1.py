368bb74e81837149f7b71452136925e0a1011799
Bharatverma4827 <Bknda1234567@gmail.com>
2024-05-14 15:20:49 +0530

    assignment uploaded

---
+++ Assignment-1.py
@@ -0,0 +1,22 @@
+#Ques 1
+state ="Rajasthan"
+country = "India"
+print(state)
+print(country)
+print(state + country)
+#Ques2
+company ="DIVINE"
+print(company.lower())
+#Ques3
+God ="krishna"
+print(len(God))
+#Ques4
+st="     hello Jecrc    "
+print(st.lstrip())
+#Ques5
+lst = [108,64,"krishna","balaram",700,18000]
+lst.insert(3,"kansa")#insert is used to put element in a list on some specific position
+print(lst)
+#append is used to insert element in last
+lst.append("parikshit")
+print(lst)
