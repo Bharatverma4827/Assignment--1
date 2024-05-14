5889dc9fc39a0573bfa338d91639973a9008142a
Bharatverma4827 <Bknda1234567@gmail.com>
2024-05-14 15:26:30 +0530

    day 2

---
+++ day-2.py
@@ -0,0 +1,38 @@
+var1 = 10
+var2 = 25.21
+var3 = True
+var4 = "upflairs"
+var5 = 'jecrc'
+var6 = """jecrc foundation"""
+print(var1)
+print(var2)
+print(var3)
+print(var4)
+print(var5)
+print(var6)
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(type(var4))
+print(type(var5))
+print(type(var6))
+print("""hello My name is Bharat 
+i am in IT branch""")
+college = "Jecrc Foundation"
+print(college.upper())
+print(college.lower())
+print(college.endswith('n'))
+print(college.startswith('J'))
+college = "  Jecrc Foundation    "
+print(college.strip())
+print(college.lstrip())
+print(college.rstrip())
+print(college[5])
+print(college[0:7])
+colllege = 'jecrc jaipur rajasthan'
+print(colllege[0:6])
+print(colllege[-2:]) 
+print(colllege[-6:-3])
+print(colllege[::-1])
+print(colllege[17: ])
+print(colllege[-1])
