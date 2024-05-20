305fe7964b1a9c6fcf3c49a64231fca808fff138
Bharatverma4827 <Bknda1234567@gmail.com>
2024-05-20 21:52:44 +0530

    Add files via upload

---
+++ assignment-2.py
@@ -0,0 +1,25 @@
+# What is github? write a simple defination for github.
+Github ='''it is a platform where we can store and share our code.
+ It helps programmers to work together on a project. 
+To access,change, and see others code.Also we can access history of our code or we may revert our code.'''
+
+# clone git hub repsitory , command and each part
+clone = ''' download and install git bash. then write command 'git clone' in it and copy the url of repository from github
+for example -git clone https://github.com/username/example_repo.git'''
+
+#Access "na" from list and change it in upper case
+fruits = ['apple','banana','cherry','date']
+print(fruits [1][2:4])
+print(fruits [1][2:4].upper())
+
+#tuple unpacking
+unpacking = "it is a feature of python that allow to convert the elements of tuple into multiple variables"
+Coordinates_tuple = (10,20,30)
+A,B,C = Coordinates_tuple
+print (A)
+print(B)
+print(C)
+
+
+
+
