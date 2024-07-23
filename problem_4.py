8b81d08bd180ee03d73f1aba04680aade91f580c
Bharatverma4827 <Bknda1234567@gmail.com>
2024-07-23 09:37:40 +0530

    Add files via upload
    Use comparison operator to find out whether ‘a’ given variable a is greater than 
    ‘b’ or not.

---
+++ problem_4.py
@@ -0,0 +1,14 @@
+a = 34
+b = 80
+
+print("A is",a)
+print("B is ",b)
+print("a is greater than b", a>b)
+
+a =int(input("Enter a No. A:\n"))
+b =int(input("Enter a No. B:\n"))
+
+print("The first No. A is\n" ,a)
+print("The first No. B is\n" ,b)
+
+print("The average of given 2 numbers is:",(a+b)/2)
